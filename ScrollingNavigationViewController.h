f704b509cd556cac06e9bd08715428e37b12c8b6
j7ios <j7na.ios@gmail.com>
2017-03-05 11:27:52 +0100

    Scrolling Navigation Controller
    A custom UINavigationController. Compatible with Swift 2.0 or next.
    Compatible with Objective-C.

---
+++ ScrollingNavigationViewController.h
@@ -0,0 +1,9 @@
+//  Created by Jack7 on 30/08/16.
+//  Copyright © 2016 GiacomoB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScrollingNavigationViewController : UIViewController<UIScrollViewDelegate>
+
+@end
