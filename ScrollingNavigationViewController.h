e647238da8ebd8115d4f32b97204248114c6a48a
j7ios <j7na.ios@gmail.com>
2017-03-05 11:26:33 +0100

    Revert "A custom UINavigationController. Compatible with Swift 2.0 or next."
    This reverts commit cca5a8c1f182de3da24b0144aa8d9deb014b01cf.

--- ScrollingNavigationViewController.h
+++
@@ -1,9 +0,0 @@
-//  Created by Jack7 on 30/08/16.
-//  Copyright © 2016 GiacomoB. All rights reserved.
-//
-
-#import <UIKit/UIKit.h>
-
-@interface ScrollingNavigationViewController : UIViewController<UIScrollViewDelegate>
-
-@end
